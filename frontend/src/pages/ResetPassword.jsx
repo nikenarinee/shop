9b00abc1d5fad7aa6d71039379566fa7b8b01a6d
nice <nicephimpha@gmail.com>
2026-03-23 13:57:57 +0700

    ตั้งรหัสใหม่

---
+++ frontend/src/pages/ResetPassword.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { supabase } from "../lib/supabase"
+import { useNavigate } from "react-router-dom"
+
+function ResetPassword() {
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  const handleReset = async () => {
+    const { error } = await supabase.auth.updateUser({
+      password: password
+    })
+
+    if (error) {
+      alert("เปลี่ยนรหัสไม่สำเร็จ")
+      console.log(error)
+    } else {
+      alert("เปลี่ยนรหัสสำเร็จ 🎉")
+      navigate("/login")
+    }
+  }
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>ตั้งรหัสผ่านใหม่</h2>
+
+      <input
+        type="password"
+        placeholder="รหัสใหม่"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <br /><br />
+
+      <button onClick={handleReset}>
+        บันทึกรหัสใหม่
+      </button>
+    </div>
+  )
+}
+
+export default ResetPassword
